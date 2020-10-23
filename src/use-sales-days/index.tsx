33871a532abb0ec9ff40c440b54bbf780aeb4c35
Joe Ng'ethe <joeynimu@gmail.com>
2020-10-23 11:44:58 +0300

    chore: log info

--- src/use-sales-days/index.tsx
+++ src/use-sales-days/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 
 const useSalesDays = (workingDays: number[]): Boolean => {
-
+  console.info({ workingDays });
   const [isASalesDay, setSalesDay] = useState(false);
 
   useEffect(() => {
+    console.info({ workingDays });
     const date = new Date();
       const today = date.getDay();
       const isTodayASalesDay = workingDays.includes(today);
